24739338fd41868a923b1ea9383376e24a3fb168
Vitor Machado <vitormds22@gmail.com>
2020-11-22 03:08:19 -0300

    Inserindo template html e renderizando na pagina

--- jogoteca/jogoteca.py
+++ jogoteca/jogoteca.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/inicio')
 
 def ola():
-    return '<h1>Olá Flask!</h1>'
+    return render_template('lista.html')
 
 app.run()
